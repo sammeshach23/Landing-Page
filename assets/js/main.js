4395dfe93bf283b9d07bb1f4db7aa432ef8af3b5
SAM MESHACH A <sammeshach333@gmail.com>
2024-06-12 10:47:10 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,13 +26,14 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 const sections = document.querySelectorAll('section[id]')
 
-function scrollActive(){
-    const scrollY = window.pageYOffset
-
-    sections.forEach(current =>{
-        const sectionHeight = current.offsetHeight
+function scrollActive() {
+    const scrollY = window.scrollY;
+    
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight;
         const sectionTop = current.offsetTop - 50;
-        sectionId = current.getAttribute('id')
+        const sectionId = current.getAttribute('id');
+        
 
         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
